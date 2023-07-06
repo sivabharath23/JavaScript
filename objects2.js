f4a104fea9f4ef13bf41e4b2d088af867b7c6455
sivabharath23 <sivabharath23@gmail.com>
2023-07-06 12:06:54 +0530

    sdfghj

--- objects2.js
+++ objects2.js
@@ -1,41 +1,41 @@
-// //object constructor
-// function MyFunction(id, name, salary) {
-//   //better to use caps in initial user can view as constructor Function
-//   this.id = id;
-//   this.name = name;
-//   this.salary = salary;
-// }
+//object constructor
+function MyFunction(id, name, salary) {
+  //better to use caps in initial user can view as constructor Function
+  this.id = id;
+  this.name = name;
+  this.salary = salary;
+}
 
-// let consobj = new MyFunction(148, "siva", 20000);
-// console.log(consobj);
+let consobj = new MyFunction(148, "siva", 20000);
+console.log(consobj);
 
-// console.log("_______________________________________");
+console.log("_______________________________________");
 
-// let obj1 = {
-//   name: "siva",
-//   id: 148,
-//   laptop: {
-//     cpu: "Ryzen 5",
-//     clockspeed: "2.4GHZ",
-//     Brand: "Asus",
-//   },
-// };
+let obj1 = {
+  name: "siva",
+  id: 148,
+  laptop: {
+    cpu: "Ryzen 5",
+    clockspeed: "2.4GHZ",
+    Brand: "Asus",
+  },
+};
 
-// console.log(obj1["laptop"]);
+console.log(obj1["laptop"]);
 
-// console.log("_______________________________________");
+console.log("_______________________________________");
 
-// for (let key in obj1) {
-//   console.log(key, obj1[key]);
-// }
+for (let key in obj1) {
+  console.log(key, obj1[key]);
+}
 
-// console.log("________________________________________");
+console.log("________________________________________");
 
-// for (let key1 in obj1.laptop) {
-//   console.log(key1, obj1.laptop[key1]);
-// }
+for (let key1 in obj1.laptop) {
+  console.log(key1, obj1.laptop[key1]);
+}
 
-//function inside object or method function
+// function inside object or method function
 let data = {
   name: "siva",
   id: 148,
--- type.js
+++ type.js
@@ -1,9 +1,2 @@
-let x = 1;
-for(let i=5;i>=1;i--){
-    x = x*i;
-}
-console.log(x);
-
-
-let a = "1234567890"
-let b = ;
+let a = "1245";
+let b
