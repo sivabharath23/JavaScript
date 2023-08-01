8f6f6e86181e0ff497322efa76a751779cd2ac0d
sivabharath23 <sivabharath23@gmail.com>
2023-08-01 16:25:52 +0530

    JS

--- filterMapReduce.js
+++ filterMapReduce.js
@@ -1,12 +1,12 @@
 let arr =[1,2,3,4,5,6]
 
-arr.filter((value) => {return value%2!=0})//first it filters the array by condition
-   .forEach((value) => {//then itearates by forEach method
+   arr.filter((value) => {return value%2!=0})//first it filters the array by condition
+      .forEach((value) => {//then itearates by forEach method
     console.log(value)
    })
 
    console.log("--------------------");
-+
+
     let res=arr.filter((n) => {return n%2==0})//first it filters the array by condition
                .map((m1) => {return m1*2})//map picks the value and change it by condition
                .reduce((a,b) =>{return a+b;})//reduces the array by previous and current value
